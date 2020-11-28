efc992588a70ed8e6dce1d575f40b1996c88834e
neozilot <neozilot@gmail.com>
2020-11-28 20:24:02 +0900

    django course 26 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse, reverse_lazy
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 
 def hello_world(request):
@@ -44,3 +44,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
