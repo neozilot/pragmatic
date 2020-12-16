51e1b622b930460c923ee5daf3e910e7b4dd8711
neozilot <neozilot@gmail.com>
2020-12-17 05:55:05 +0900

    django course 60 commit

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,14 @@ WORKDIR /home/pragmatic/
 
 RUN pip install -r requirements.txt
 
+RUN pip install gunicorn
+
 RUN echo "SECRET_KEY=)u^slmj*#-97%vbo=j#y=il47j^#_$b^^m0r4a0*b-65qnth7(" > .env
 
 RUN python manage.py migrate
 
+RUN python manage.py collectstatic
+
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["gunicorn", "pragmatic.wsgi", "--bind", "0.0.0.0:8000"]
--- pragmatic/settings/base.py
+++ pragmatic/settings/base.py
@@ -16,31 +16,10 @@ import os, environ
 
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -95,12 +74,6 @@ WSGI_APPLICATION = 'pragmatic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
